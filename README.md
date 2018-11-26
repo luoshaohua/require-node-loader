d2e64ee09a7326f5f44b68c30bed961c8d7869d1
luoshaohua <luoshaohua.cn@gmail.com>
2018-11-26 20:21:55 +0800

    readme

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ webpack loader for npm package require-node
 
 Please refer to npm package: 
 [require-node](https://www.npmjs.com/package/require-node)
-[require-node-example](https://www.npmjs.com/package/require-node-example)
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var _browserify = require('require-node/_browserify');
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
-    // console.log(typeof this.query, this.query);
     try {
         return _browserify.toCommonJS(this.resourcePath, this.query);
     } catch (err) {
