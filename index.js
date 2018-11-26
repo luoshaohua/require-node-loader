71fcbb58f4874eb277ba82c20b24d5daa11efb19
luoshaohua <luoshaohua.cn@gmail.com>
2018-11-26 16:39:19 +0800

    cancle log

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var _browserify = require('require-node/_browserify');
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
-    console.log(typeof this.query, this.query);
+    // console.log(typeof this.query, this.query);
     try {
         return _browserify.toCommonJS(this.resourcePath, this.query);
     } catch (err) {
